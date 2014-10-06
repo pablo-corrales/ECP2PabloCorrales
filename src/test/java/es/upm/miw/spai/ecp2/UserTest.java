45551ec9f8fa9cb0dfef9b2ed80b10595043a6a9
pablo-corrales <charliequiro>
2014-10-07 00:39:21 +0200

    Commit correccion a Clase UserTest

--- src/test/java/es/upm/miw/spai/ecp2/UserTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/UserTest.java
@@ -8,16 +8,16 @@ import org.junit.Test;
 public class UserTest {
     private User user;
     private static final String NAME="john";
-    private static final String NAME_UPPER="JOHN";
-    private static final String FAMILY_NAME_UPPER="LENON";
+    private static final String NAME_UPPER="John";
+    private static final String FAMILY_NAME_UPPER="Lenon";
     private static final String FAMILY_NAME="lenon";
     @Before
      public void before() {
-         user= new User(111, NAME, FAMILY_NAME);
+         user= new User(1, NAME, FAMILY_NAME);
      }
     @Test
     public void testUser() {
-        assertEquals(111, user.getNumber());
+        assertEquals(1, user.getNumber());
         assertEquals("john", user.getName());    
         assertEquals("lenon", user.getFamilyName());     
     }
