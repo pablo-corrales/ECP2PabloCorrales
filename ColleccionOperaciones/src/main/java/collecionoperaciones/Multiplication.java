d79c57891c83929b4c39cb0790ebb71796f457cf
pablo-corrales <charliequiro>
2014-10-19 15:09:01 +0200

    Se cambia nombre de clase Addition por Summation en todas las clases en donde se utiliza el nombre de la clase Addition. Se aumenta clase Multiplication

---
+++ ColleccionOperaciones/src/main/java/collecionoperaciones/Multiplication.java
@@ -0,0 +1,5 @@
+package collecionoperaciones;
+
+public class Multiplication {
+
+}
--- ColleccionOperaciones/src/main/java/collecionoperaciones/Summation.java
+++ ColleccionOperaciones/src/main/java/collecionoperaciones/Summation.java
@@ -1,9 +1,9 @@
 package collecionoperaciones;
 
-public class Addition {
+public class Summation {
     private final int operator1, operator2;
 
-    public Addition(final int operator1, final int operator2) {
+    public Summation(final int operator1, final int operator2) {
         this.operator1 = operator1;
         this.operator2 = operator2;
     }
